67089884468c9af9e2ae2acd106ad32adbb87cfc
Yunseop Kim <c.o.d.e@kakao.com>
2022-08-14 22:17:10 +0900

    refactor: to REST api

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,40 +1,58 @@
-import { authService, dbService } from "../fbase";
-import { useEffect, useState } from "react";
+import { authService } from "../fbase";
 import Nweet from "../components/Nweet";
 import NweetFactory from "../components/NweetFactory";
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
 import { useAuthUser } from "../hooks/quries/useAuthUser";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Nweet {
+  id: string;
+  text: string;
+  createdAt: number;
+  creatorId: string;
+  attachmentUrl: string;
+}
 
 const Home = ({ userObj }: any) => {
-  const [nweets, setNweets] = useState<any[]>([]);
   const user = useAuthUser(['user'], authService, {
     select: (data) => ({
       uid: data?.uid ?? '',
       displayName: data?.displayName ?? '',
     })
   });
-
-  useEffect(() => {
-    getDocs(
-      query(
-        collection(dbService, "nweets"),
-        orderBy("createdAt", "desc"),
-      )
-    ).then((snapshot) => {
-      const newArray: any = snapshot.docs.map((document) => ({
-        id: document.id,
-        ...document.data(),
-      }));
-      setNweets(newArray);
-    });
-
-  }, []);
+  const nweets = useQuery<Nweet[], Error>(['nweets'], {
+    queryFn: () => axios.post('https://firestore.googleapis.com/v1/projects/tablelab-d9e2e/databases/(default)/documents:runQuery', {
+      structuredQuery:
+      {
+        from: [
+          {
+            collectionId: 'nweets'
+          }
+        ],
+        orderBy: [
+          {
+            field: {
+              fieldPath: 'createdAt'
+            },
+            direction: 'DESCENDING'
+          }
+        ],
+      }
+    })
+      .then(({ data }) => data.map((item: any) => item.document).map(((item: any) => ({
+        id: item.name,
+        text: item.fields.text.stringValue,
+        createdAt: Number(item.fields.createdAt.integerValue),
+        creatorId: item.fields.creatorId.stringValue,
+        attachmentUrl: item.fields.attachmentUrl.stringValue,
+      }))))
+  })
 
   return (
     <div className="container">
       <NweetFactory />
       <div style={{ marginTop: 30 }}>
-        {nweets.map((nweet) => (
+        {nweets.data?.map((nweet) => (
           <Nweet
             key={nweet.id}
             nweetObj={nweet}
