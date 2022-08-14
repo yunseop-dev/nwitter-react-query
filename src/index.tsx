dc65add1037bb3c32ae7ba85c043d61fc365e2df
Yunseop Kim <c.o.d.e@kakao.com>
2022-08-14 22:41:49 +0900

    fix: refetchOnWindowFocus

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,13 @@ import reportWebVitals from './reportWebVitals';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
