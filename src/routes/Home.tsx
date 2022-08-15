e2932b5eedaac4918c692ceb12f4592c6accabf7
Yunseop Kim <c.o.d.e@kakao.com>
2022-08-15 21:25:49 +0900

    refactor: useNweetsQuery

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -5,6 +5,8 @@ import { useAuthUser } from "../hooks/quries/useAuthUser";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import last from "lodash/last";
+import { useQueryParam, NumberParam, StringParam } from "use-query-params";
+import { Link } from "react-router-dom";
 
 export interface INweet {
   id: string;
@@ -14,46 +16,47 @@ export interface INweet {
   attachmentUrl: string;
 }
 
-const Home = ({ userObj }: any) => {
+const LIMIT = 5;
+
+function useNweetsQuery({ limit = LIMIT, pageToken }: { limit: number; pageToken?: string; }) {
+  return useQuery<{ list: INweet[]; nextPageToken: string; }, Error>(['nweets', { pageToken, limit }], {
+    queryFn: () => axios.get('https://firestore.googleapis.com/v1/projects/tablelab-d9e2e/databases/(default)/documents/nweets', {
+      params: {
+        pageSize: limit,
+        orderBy: 'createdAt desc',
+        pageToken
+      }
+    }).then(({ data }) => {
+      const list = data.documents.map(((item: any) => ({
+        id: last((item.name as string).split('/')),
+        text: item.fields.text.stringValue,
+        createdAt: Number(item.fields.createdAt.integerValue),
+        creatorId: item.fields.creatorId.stringValue,
+        attachmentUrl: item.fields.attachmentUrl.stringValue,
+      })));
+      const nextPageToken = data.nextPageToken;
+      return { list, nextPageToken }
+    })
+  })
+}
+
+const Home = () => {
   const user = useAuthUser(['user'], authService, {
     select: (data) => ({
       uid: data?.uid ?? '',
       displayName: data?.displayName ?? '',
     })
   });
-  const nweets = useQuery<INweet[], Error>(['nweets'], {
-    queryFn: () => axios.post('https://firestore.googleapis.com/v1/projects/tablelab-d9e2e/databases/(default)/documents:runQuery', {
-      structuredQuery:
-      {
-        from: [
-          {
-            collectionId: 'nweets'
-          }
-        ],
-        orderBy: [
-          {
-            field: {
-              fieldPath: 'createdAt'
-            },
-            direction: 'DESCENDING'
-          }
-        ],
-      }
-    })
-      .then(({ data }) => data.map((item: any) => item.document).map(((item: any) => ({
-        id: last((item.name as string).split('/')),
-        text: item.fields.text.stringValue,
-        createdAt: Number(item.fields.createdAt.integerValue),
-        creatorId: item.fields.creatorId.stringValue,
-        attachmentUrl: item.fields.attachmentUrl.stringValue,
-      }))))
-  })
+  const [pageToken] = useQueryParam('pageToken', StringParam);
+  const [limit] = useQueryParam('limit', NumberParam);
+  const nweets = useNweetsQuery({ pageToken: pageToken ?? undefined, limit: limit ?? 5 })
+  const nextPageToken = nweets.data?.nextPageToken;
 
   return (
     <div className="container">
       <NweetFactory />
       <div style={{ marginTop: 30 }}>
-        {nweets.data?.map((nweet) => (
+        {nweets.data?.list.map((nweet) => (
           <Nweet
             key={nweet.id}
             nweetObj={nweet}
@@ -61,6 +64,7 @@ const Home = ({ userObj }: any) => {
           />
         ))}
       </div>
+      <Link to={{ search: `pageToken=${nextPageToken}` }}>Next</Link>
     </div>
   );
 };
