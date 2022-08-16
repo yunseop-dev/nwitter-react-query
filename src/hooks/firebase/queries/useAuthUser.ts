db697145ca54207a61dcf08d1ef1daea400ed3df
Yunseop Kim <yunseop.kim@actionpower.kr>
2022-08-16 16:25:06 +0900

    fix: types

--- src/hooks/firebase/queries/useAuthUser.ts
+++ src/hooks/firebase/queries/useAuthUser.ts
@@ -2,15 +2,15 @@ import { QueryKey, UseQueryOptions, UseQueryResult } from '@tanstack/react-query
 import { Auth, AuthError, NextOrObserver, User } from "firebase/auth";
 import { useSubscription } from "./useSubscription";
 
-export function useAuthUser<R = User>(
+export function useAuthUser<R = User | null>(
     queryKey: QueryKey,
     auth: Auth,
-    options: Omit<UseQueryOptions<User, AuthError, R>, "queryFn"> = {}
+    options: Omit<UseQueryOptions<User | null, AuthError, R>, "queryFn"> = {}
 ): UseQueryResult<R, AuthError> {
     const subscribeFn = (cb: NextOrObserver<User | null>) =>
         auth.onAuthStateChanged(cb);
 
-    return useSubscription<User, AuthError, R>(
+    return useSubscription<User | null, AuthError, R>(
         queryKey,
         ["useAuthUser"],
         subscribeFn,
