40c2afb3fdbf9ffe6459b2e289c24183afde02e9
Yunseop Kim <yunseop.kim@actionpower.kr>
2022-08-16 10:20:15 +0900

    remove use-query-params

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,6 +1,4 @@
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import { QueryParamProvider } from "use-query-params";
-import { ReactRouter5Adapter } from 'use-query-params/adapters/react-router-5';
 import useUser from "../hooks/queries/useUser";
 import Auth from "../pages/auth";
 import Home from "../pages/home";
@@ -11,34 +9,32 @@ const AppRouter = () => {
   const { isLoggedIn } = useUser()
   return (
     <Router>
-      <QueryParamProvider adapter={ReactRouter5Adapter}>
-        {isLoggedIn && <Navigation />}
-        <Switch>
-          {isLoggedIn ? (
-            <div
-              style={{
-                maxWidth: 890,
-                width: "100%",
-                margin: "0 auto",
-                marginTop: 80,
-                display: "flex",
-                justifyContent: "center",
-              }}
-            >
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route exact path="/profile">
-                <Profile />
-              </Route>
-            </div>
-          ) : (
+      {isLoggedIn && <Navigation />}
+      <Switch>
+        {isLoggedIn ? (
+          <div
+            style={{
+              maxWidth: 890,
+              width: "100%",
+              margin: "0 auto",
+              marginTop: 80,
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
             <Route exact path="/">
-              <Auth />
+              <Home />
             </Route>
-          )}
-        </Switch>
-      </QueryParamProvider>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+          </div>
+        ) : (
+          <Route exact path="/">
+            <Auth />
+          </Route>
+        )}
+      </Switch>
     </Router>
   );
 };
