b056b62bb11439713543c157eee9956dc4b6a997
Yunseop Kim <yunseop.kim@actionpower.kr>
2022-08-17 10:16:26 +0900

    init: tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "./styles.css";
+import "./index.css";
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
